9f48769050ae2ca3b580bd10b3dfa6ff7d4c00b1
xinjie-just <18227752005@qq.com>
2017-05-21 18:56:19 +0800

    完成所有页面

--- script/education-phone.js
+++ script/education-phone.js
@@ -29,28 +29,28 @@ $("#toTop").click(function () {
 });
 
 /*===设置文章格式===*/
-	$(".details-article").css("fontSize", "14px");
-	// 删除图片宽高属性
-	$(".article img").removeAttr("width");
-	$(".article img").removeAttr("height");
-	// 设置图片样式
-	$(".article img").css({
-		display: "block",
-		maxWidth: "100%",
-		height: "auto",
-		margin: "15px auto 0"
-	});
-	$(".article img + img").css("marginTop", "8px");
-	// 设置段落样式
-	$(".article > div, .article > p").css({
-		fontSize: "14px",
-		textIndent: "2.2em",
-		letterSpacing: ".1em",
-		lineHeight: 1.8,
-		marginTop: "7px",
-		textAlign: "justify",
-		wordWrap: "breakWord"	
-	});
-	// 删除段落中多余的换行
-	$(".article:has(br) br").remove();
-	$(".article > div:has(br) br").remove();
+$("article").css("fontSize", "14px");
+// 删除图片和多媒体宽高属性
+$("article img, article iframe, article embed, article video, article audio").removeAttr("width");
+$("article img, article iframe, article embed, article video, article audio").removeAttr("height");
+// 设置图片和多媒体样式
+$("article img, article iframe, article embed, article video, article audio").css({
+	display: "block",
+	maxWidth: "100%",
+	height: "auto",
+	margin: "15px auto 0"
+});
+$("article img + img").css("marginTop", "8px");
+// 设置段落样式
+$("article > div, article > p").css({
+	fontSize: "14px",
+	textIndent: "2.2em",
+	letterSpacing: ".1em",
+	lineHeight: 1.8,
+	marginTop: "7px",
+	textAlign: "justify",
+	wordWrap: "breakWord"	
+});
+// 删除段落中多余的换行
+$("article:has(br) br").remove();
+$("article > div:has(br) br").remove();
